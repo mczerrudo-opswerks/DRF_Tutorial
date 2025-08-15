da27f9f72840ec996c4e6ea896bf8e1475474e0e
Mc Clareenz Zerrudo <mc.zerrudo@ssed-academy.opswerks.com>
2025-08-15 15:34:56 +0800

    Added Menu Test

--- restaurantApi/core/views.py
+++ restaurantApi/core/views.py
@@ -13,6 +13,7 @@ from core.serializers import (
 )
 from rest_framework import permissions
 from core.tasks import send_order_created_email
+from rest_framework.exceptions import PermissionDenied
 
 logger = logging.getLogger('restaurantAPI')
 
@@ -25,6 +26,7 @@ class MenuItemListCreateAPIView(generics.ListCreateAPIView):
     """
     queryset = MenuItem.objects.order_by('pk').select_related("restaurant")
     serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     # Caching 
     @method_decorator(cache_page(60 * 15, key_prefix = 'menu_list')) # connected with signals.py
@@ -32,9 +34,15 @@ class MenuItemListCreateAPIView(generics.ListCreateAPIView):
         return super().list(request, *args, **kwargs)
     
     def get_queryset(self):
-        import time
-        time.sleep(2)
+        # import time
+        # time.sleep(2)
         return super().get_queryset()
+    
+    def perform_create(self, serializer):
+        restaurant = serializer.validated_data.get("restaurant")
+        if restaurant.owner_id != self.request.user.id:
+            raise PermissionDenied("You can only add menu items to your own restaurant.")
+        serializer.save()
 
 class MenuItemRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     """
